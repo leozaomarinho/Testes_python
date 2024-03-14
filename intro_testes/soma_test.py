055bf98e590afe2211e30b9ef42fded512404887
Leonardo Marinho <leowalkg6@gmail.com>
2024-03-13 23:45:16 -0300

    refatorando

--- intro_testes/soma_test.py
+++ intro_testes/soma_test.py
@@ -1,10 +1,13 @@
 from soma_calc import soma,divisao
 
-def test_soma(soma):
+
+class Matematica():
+    def test_soma(soma):
+        
+        sum = soma(2,4)
+        assert sum == 6
+        
+    def test_divisao(divisao):
+        result = divisao(4,4)
+        assert result == 1
     
-    sum = soma(2,4)
-    assert sum == 6
-    
-def test_divisao(divisao):
-    result = divisao(4,4)
-    assert result == 1
